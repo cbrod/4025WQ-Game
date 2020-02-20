8f76b57897b6a5562766dc96ab6484da7ed46d46
Mike Koenig <koenigm@seattleu.edu>
2020-02-20 09:13:58 -0800

    Added Character Create Page

---
+++ Game/Game/Views/Characters/CharacterCreatePage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using Game.Helpers;
+using Game.Models;
+using Game.ViewModels;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// Create Character
+    /// </summary>
+    [DesignTimeVisible(false)]
+    public partial class CharacterCreatePage : ContentPage
+    {
+        // The Character to create
+        public GenericViewModel<CharacterModel> ViewModel { get; set; }
+
+        // Empty Constructor for UTs
+        public CharacterCreatePage(bool UnitTest){}
+
+        /// <summary>
+        /// Constructor for Create makes a new model
+        /// </summary>
+        public CharacterCreatePage(GenericViewModel<CharacterModel> data)
+        {
+            InitializeComponent();
+
+            data.Data = new CharacterModel();
+
+            BindingContext = this.ViewModel = data;
+
+            this.ViewModel.Title = "Create";
+
+            // Load the values for the Level into the Picker
+            for (var i = 1; i <= LevelTableHelper.MaxLevel; i++)
+            {
+                LevelPicker.Items.Add(i.ToString());
+            }
+
+            MaxHealthValue.Text = string.Format(" : {0:G}", ViewModel.Data.MaxHealth);
+        }
+
+        /// <summary>
+        /// The Level selected from the list
+        /// Need to recalculate Max Health
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void Level_Changed(object sender, EventArgs args)
+        {
+            var level = LevelPicker.SelectedIndex + 1;
+
+            // Roll the Dice and reset the Health
+            ViewModel.Data.MaxHealth = DiceHelper.RollDice(level, 10);
+
+            MaxHealthValue.Text = string.Format(" : {0:G}", ViewModel.Data.MaxHealth);
+        }
+
+        /// <summary>
+        /// Save by calling for Create
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Save_Clicked(object sender, EventArgs e)
+        {
+            // If the image in the data box is empty, use the default one..
+            if (string.IsNullOrEmpty(ViewModel.Data.ImageURI))
+            {
+                ViewModel.Data.ImageURI = new CharacterModel().ImageURI;
+            }
+
+            MessagingCenter.Send(this, "Create", ViewModel.Data);
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Cancel the Create
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
