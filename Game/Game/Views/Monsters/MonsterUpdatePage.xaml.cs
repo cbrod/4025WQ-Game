e99e655db282c795954d03d0d96ae4e0b18748e0
Mike Koenig <koenigm@seattleu.edu>
2020-02-24 11:03:15 -0800

    Updated Monster Update Page xaml and cs for Difficulty Picker

--- Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
@@ -37,6 +37,12 @@ namespace Game.Views
 
             this.ViewModel.Title = "Update " + data.Title;
 
+            // Load the values for the Diffculty into the Picker
+            foreach (var item in DifficultyEnumHelper.GetListMonster)
+            {
+                DifficultyPicker.Items.Add(item);
+            }
+
             UpdatePageBindingContext();
         }
 
@@ -46,14 +52,37 @@ namespace Game.Views
         /// <returns></returns>
         public bool UpdatePageBindingContext()
         {
-            // Temp store off the Level
-            var level = this.ViewModel.Data.Level;
+            // Temp store off the Difficulty
+            var difficulty = this.ViewModel.Data.Difficulty;
 
             // Clear the Binding and reset it
             BindingContext = null;
             BindingContext = this.ViewModel;
 
+            ViewModel.Data.Difficulty = difficulty;
+
+            DifficultyPicker.SelectedItem = difficulty;
+            DifficultyPicker.SelectedIndex = DifficultyPicker.Items.IndexOf(DifficultyPicker.SelectedItem.ToString());
+
             return true;
+        }
+
+        /// <summary>
+        /// The Level selected from the list
+        /// Need to recalculate Max Health
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void Difficulty_Changed(object sender, EventArgs args)
+        {
+            // Check for null, SelectedItem is not set when the control is created
+            if (DifficultyPicker.SelectedItem == null)
+            {
+                return; 
+            }
+
+            // Change the Difficulty
+            ViewModel.Data.Difficulty = DifficultyEnumHelper.ConvertStringToEnum(DifficultyPicker.SelectedItem.ToString());
         }
 
         /// <summary>
