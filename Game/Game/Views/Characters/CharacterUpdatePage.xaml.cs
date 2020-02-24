965df85def64741f9679fc459adf9f2ae5a0a767
Mike Koenig <koenigm@seattleu.edu>
2020-02-23 21:27:05 -0800

    Updated Character Update Page

--- Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
+++ Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
@@ -7,6 +7,8 @@ using Xamarin.Forms.Xaml;
 using Game.ViewModels;
 using Game.Models;
 using Game.Helpers;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Game.Views
 {
@@ -17,14 +19,17 @@ namespace Game.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CharacterUpdatePage : ContentPage
     {
-        // View Model for Character
-        public readonly GenericViewModel<CharacterModel> ViewModel;
+        // The Character to create
+        public GenericViewModel<CharacterModel> ViewModel { get; set; }
 
-        // Empty Constructor for Tests
-        public CharacterUpdatePage(bool UnitTest){ }
+        // Hold the current location selected
+        public ItemLocationEnum PopupLocationEnum = ItemLocationEnum.Unknown;
+
+        // Empty Constructor for UTs
+        public CharacterUpdatePage(bool UnitTest) { }
 
         /// <summary>
-        /// Constructor that takes and existing data Character
+        /// Constructor for Create makes a new model
         /// </summary>
         public CharacterUpdatePage(GenericViewModel<CharacterModel> data)
         {
@@ -34,14 +39,37 @@ namespace Game.Views
 
             this.ViewModel.Title = "Update " + data.Title;
 
-
             // Load the values for the Level into the Picker
             for (var i = 1; i <= LevelTableHelper.MaxLevel; i++)
             {
                 LevelPicker.Items.Add(i.ToString());
             }
 
-            MaxHealthValue.Text = string.Format(" : {0:G}", ViewModel.Data.MaxHealth);
+            UpdatePageBindingContext();
+        }
+
+        /// <summary>
+        /// Redo the Binding to cause a refresh
+        /// </summary>
+        /// <returns></returns>
+        public bool UpdatePageBindingContext()
+        {
+            // Temp store off the Level
+            var level = this.ViewModel.Data.Level;
+
+            // Clear the Binding and reset it
+            BindingContext = null;
+            BindingContext = this.ViewModel;
+
+            // This resets the Picker to -1 index, need to reset it back
+            ViewModel.Data.Level = level;
+            LevelPicker.SelectedIndex = ViewModel.Data.Level - 1;
+
+            ManageHealth();
+
+            AddItemsToDisplay();
+
+            return true;
         }
 
         /// <summary>
@@ -52,16 +80,26 @@ namespace Game.Views
         /// <param name="args"></param>
         public void Level_Changed(object sender, EventArgs args)
         {
-            var level = LevelPicker.SelectedIndex + 1;
+            // Change the Level
+            ViewModel.Data.Level = LevelPicker.SelectedIndex + 1;
 
-            // Roll the Dice and reset the Health
-            ViewModel.Data.MaxHealth = DiceHelper.RollDice(level, 10);
-
-            MaxHealthValue.Text = string.Format(" : {0:G}", ViewModel.Data.MaxHealth);
+            ManageHealth();
         }
 
         /// <summary>
-        /// Save calls to Update
+        /// Change the Level Picker
+        /// </summary>
+        public void ManageHealth()
+        {
+            // Roll for new HP
+            ViewModel.Data.MaxHealth = RandomPlayerHelper.GetHealth(ViewModel.Data.Level);
+
+            // Show the Result
+            MaxHealthValue.Text = ViewModel.Data.MaxHealth.ToString();
+        }
+
+        /// <summary>
+        /// Save by calling for Create
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -73,12 +111,13 @@ namespace Game.Views
                 ViewModel.Data.ImageURI = new CharacterModel().ImageURI;
             }
 
-            MessagingCenter.Send(this, "Update", ViewModel.Data);
+            MessagingCenter.Send(this, "Create", ViewModel.Data);
+
             await Navigation.PopModalAsync();
         }
 
         /// <summary>
-        /// Cancel and close this page
+        /// Cancel the Create
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -115,6 +154,234 @@ namespace Game.Views
         public void Speed_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
         {
             SpeedValue.Text = String.Format("{0}", e.NewValue);
+        }
+
+        /// <summary>
+        /// The row selected from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnPopupItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            ItemModel data = args.SelectedItem as ItemModel;
+            if (data == null)
+            {
+                return;
+            }
+
+            ViewModel.Data.AddItem(PopupLocationEnum, data.Id);
+
+            AddItemsToDisplay();
+
+            ClosePopup();
+        }
+
+        /// <summary>
+        /// Show the Popup for Selecting Items
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        public bool ShowPopup(ItemLocationEnum location)
+        {
+            PopupItemSelector.IsVisible = true;
+
+            PopupLocationLabel.Text = "Items for :";
+            PopupLocationValue.Text = location.ToMessage();
+
+            // Make a fake item for None
+            var NoneItem = new ItemModel
+            {
+                Id = null, // will use null to clear the item
+                Guid = "None", // how to find this item amoung all of them
+                ImageURI = "icon_cancel.png",
+                Name = "None",
+                Description = "None"
+            };
+
+            List<ItemModel> itemList = new List<ItemModel>
+            {
+                NoneItem
+            };
+
+            // Add the rest of the items to the list
+            itemList.AddRange(ItemIndexViewModel.Instance.GetLocationItems(location));
+
+            // Populate the list with the items
+            PopupLocationItemListView.ItemsSource = itemList;
+
+            // Remember the location for this popup
+            PopupLocationEnum = location;
+
+            return true;
+        }
+
+        /// <summary>
+        /// When the user clicks the close in the Popup
+        /// hide the view
+        /// show the scroll view
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void ClosePopup_Clicked(object sender, EventArgs e)
+        {
+            ClosePopup();
+        }
+
+        /// <summary>
+        /// Close the popup
+        /// </summary>
+        public void ClosePopup()
+        {
+            PopupItemSelector.IsVisible = false;
+        }
+
+        /// <summary>
+        /// Show the Items the Character has
+        /// </summary>
+        public void AddItemsToDisplay()
+        {
+            var FlexList = ItemBox.Children.ToList();
+            foreach (var data in FlexList)
+            {
+                ItemBox.Children.Remove(data);
+            }
+
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Head));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Necklass));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.PrimaryHand));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.OffHand));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.RightFinger));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.LeftFinger));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Feet));
+        }
+
+        /// <summary>
+        /// Look up the Item to Display
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        public StackLayout GetItemToDisplay(ItemLocationEnum location)
+        {
+            // Get the Item, if it exist show the info
+            // If it does not exist, show a Plus Icon for the location
+
+            // Defualt Image is the Plus
+            var ImageSource = "icon_add.png";
+
+            var data = ViewModel.Data.GetItemByLocation(location);
+            if (data == null)
+            {
+                data = new ItemModel { Location = location, ImageURI = ImageSource };
+            }
+
+            // Hookup the Image Button to show the Item picture
+            var ItemButton = new ImageButton
+            {
+                Style = (Style)Application.Current.Resources["ImageMediumStyle"],
+                Source = data.ImageURI
+            };
+
+            // Add a event to the user can click the item and see more
+            ItemButton.Clicked += (sender, args) => ShowPopup(data.Location);
+
+            // Add the Display Text for the item
+            var ItemLabel = new Label
+            {
+                Text = location.ToMessage(),
+                Style = (Style)Application.Current.Resources["ValueStyleMicro"],
+                HorizontalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center
+            };
+
+            // Put the Image Button and Text inside a layout
+            var ItemStack = new StackLayout
+            {
+                Padding = 3,
+                Style = (Style)Application.Current.Resources["ItemImageBox"],
+                HorizontalOptions = LayoutOptions.Center,
+                Children = {
+                    ItemButton,
+                    ItemLabel
+                },
+            };
+
+            return ItemStack;
+        }
+
+        /// <summary>
+        /// Randomize Character Values and Items
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void RollDice_Clicked(object sender, EventArgs e)
+        {
+            DiceAnimationHandeler();
+
+            RandomizeCharacter();
+
+            return;
+        }
+
+        /// <summary>
+        /// Randomize the Character, keep the level the same
+        /// </summary>
+        /// <returns></returns>
+        public bool RandomizeCharacter()
+        {
+            // Randomize Name
+            ViewModel.Data.Name = RandomPlayerHelper.GetCharacterName();
+            ViewModel.Data.Description = RandomPlayerHelper.GetCharacterDescription();
+
+            // Randomize the Attributes
+            ViewModel.Data.Attack = RandomPlayerHelper.GetAbilityValue();
+            ViewModel.Data.Speed = RandomPlayerHelper.GetAbilityValue();
+            ViewModel.Data.Defense = RandomPlayerHelper.GetAbilityValue();
+
+            // Randomize an Item for Location
+            ViewModel.Data.Head = RandomPlayerHelper.GetItem(ItemLocationEnum.Head);
+            ViewModel.Data.Necklass = RandomPlayerHelper.GetItem(ItemLocationEnum.Necklass);
+            ViewModel.Data.PrimaryHand = RandomPlayerHelper.GetItem(ItemLocationEnum.PrimaryHand);
+            ViewModel.Data.OffHand = RandomPlayerHelper.GetItem(ItemLocationEnum.OffHand);
+            ViewModel.Data.RightFinger = RandomPlayerHelper.GetItem(ItemLocationEnum.Finger);
+            ViewModel.Data.LeftFinger = RandomPlayerHelper.GetItem(ItemLocationEnum.Finger);
+            ViewModel.Data.Feet = RandomPlayerHelper.GetItem(ItemLocationEnum.Feet);
+
+            ViewModel.Data.MaxHealth = RandomPlayerHelper.GetHealth(ViewModel.Data.Level);
+
+            UpdatePageBindingContext();
+
+            return true;
+        }
+
+        /// <summary>
+        /// Setup the Dice Animation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public bool DiceAnimationHandeler()
+        {
+            // Animate the Rolling of the Dice
+            ImageButton image = RollDice;
+            uint duration = 1000;
+
+            var parentAnimation = new Animation();
+
+            // Grow the image Size
+            var scaleUpAnimation = new Animation(v => image.Scale = v, 1, 2, Easing.SpringIn);
+
+            // Spin the Image
+            var rotateAnimation = new Animation(v => image.Rotation = v, 0, 360);
+
+            // Shrink the Image
+            var scaleDownAnimation = new Animation(v => image.Scale = v, 2, 1, Easing.SpringOut);
+
+            parentAnimation.Add(0, 0.5, scaleUpAnimation);
+            parentAnimation.Add(0, 1, rotateAnimation);
+            parentAnimation.Add(0.5, 1, scaleDownAnimation);
+
+            parentAnimation.Commit(this, "ChildAnimations", 16, duration, null, null);
+
+            return true;
         }
     }
 }
