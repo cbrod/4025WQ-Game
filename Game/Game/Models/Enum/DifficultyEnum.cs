828ee6ba3d69c78d9575dd9bb2640c0ede303c2e
Mike Koenig <koenigm@seattleu.edu>
2020-02-23 23:13:14 -0800

    Adding Difficulty Enum and UTs

---
+++ Game/Game/Models/Enum/DifficultyEnum.cs
@@ -0,0 +1,73 @@
+﻿namespace Game.Models
+{
+    /// <summary>
+    /// The Types of Difficultys
+    /// Used by Item to specify what it modifies.
+    /// </summary>
+    public enum DifficultyEnum
+    {
+        // Not specified
+        Unknown = 0,    
+
+        // Easier than mostThe speed of the character, impacts movement, and initiative
+        Easy = 10,
+
+        // Average
+        Average = 12,
+
+        // Hard
+        Hard = 14,
+
+        // Harder than Hard
+        Difficult = 16,
+
+        // The highest value
+        Impossible= 18,
+    }
+
+    /// <summary>
+    /// Friendly strings for the Enum Class
+    /// </summary>
+    public static class DifficultyEnumExtensions
+    {
+        /// <summary>
+        /// Display a String for the Enums
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ToMessage(this DifficultyEnum value)
+        {
+            // Default String
+            var Message = "Unknown";
+
+            switch (value)
+            {
+                case DifficultyEnum.Easy:
+                    Message = "Easy";
+                    break;
+
+                case DifficultyEnum.Average:
+                    Message = "Average";
+                    break;
+
+                case DifficultyEnum.Hard:
+                    Message = "Hard";
+                    break;
+
+                case DifficultyEnum.Difficult:
+                    Message = "Harder than Hard";
+                    break;
+
+                case DifficultyEnum.Impossible:
+                    Message = "Impossible";
+                    break;
+
+                case DifficultyEnum.Unknown:
+                default:
+                    break;
+            }
+
+            return Message;
+        }
+    }
+}
---
+++ UnitTests/Models/Enum/DifficultyEnumExtensionsTests.cs
@@ -0,0 +1,94 @@
+﻿using NUnit.Framework;
+
+using Game.Models;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class DifficultyEnumExtensionsTests
+    {
+        [Test]
+        public void DifficultyEnumExtensionsTests_Unknown_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = DifficultyEnum.Unknown.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Unknown", result);
+        }
+
+        [Test]
+        public void DifficultyEnumExtensionsTests_Easy_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = DifficultyEnum.Easy.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Easy", result);
+        }
+
+        [Test]
+        public void DifficultyEnumExtensionsTests_Average_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = DifficultyEnum.Average.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Average", result);
+        }
+
+        [Test]
+        public void DifficultyEnumExtensionsTests_Difficult_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = DifficultyEnum.Difficult.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Harder than Hard", result);
+        }
+
+        [Test]
+        public void DifficultyEnumExtensionsTests_Hard_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = DifficultyEnum.Hard.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Hard", result);
+        }
+
+        [Test]
+        public void DifficultyEnumExtensionsTests_Impossible_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = DifficultyEnum.Impossible.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Impossible", result);
+        }
+    }
+}
