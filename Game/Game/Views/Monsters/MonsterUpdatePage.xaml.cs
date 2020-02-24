961d5f543ce1f5b42093ee7be646db49736dce29
Mike Koenig <koenigm@seattleu.edu>
2020-02-23 22:13:52 -0800

    Updated Monster Update Page

--- Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
@@ -7,8 +7,6 @@ using Xamarin.Forms.Xaml;
 using Game.ViewModels;
 using Game.Models;
 using Game.Helpers;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace Game.Views
 {
@@ -39,12 +37,6 @@ namespace Game.Views
 
             this.ViewModel.Title = "Update " + data.Title;
 
-            // Load the values for the Level into the Picker
-            for (var i = 1; i <= LevelTableHelper.MaxLevel; i++)
-            {
-                LevelPicker.Items.Add(i.ToString());
-            }
-
             UpdatePageBindingContext();
         }
 
@@ -61,41 +53,7 @@ namespace Game.Views
             BindingContext = null;
             BindingContext = this.ViewModel;
 
-            // This resets the Picker to -1 index, need to reset it back
-            ViewModel.Data.Level = level;
-            LevelPicker.SelectedIndex = ViewModel.Data.Level - 1;
-
-            ManageHealth();
-
-            AddItemsToDisplay();
-
             return true;
-        }
-
-        /// <summary>
-        /// The Level selected from the list
-        /// Need to recalculate Max Health
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="args"></param>
-        public void Level_Changed(object sender, EventArgs args)
-        {
-            // Change the Level
-            ViewModel.Data.Level = LevelPicker.SelectedIndex + 1;
-
-            ManageHealth();
-        }
-
-        /// <summary>
-        /// Change the Level Picker
-        /// </summary>
-        public void ManageHealth()
-        {
-            // Roll for new HP
-            ViewModel.Data.MaxHealth = RandomPlayerHelper.GetHealth(ViewModel.Data.Level);
-
-            // Show the Result
-            MaxHealthValue.Text = ViewModel.Data.MaxHealth.ToString();
         }
 
         /// <summary>
@@ -155,159 +113,7 @@ namespace Game.Views
         {
             SpeedValue.Text = String.Format("{0}", e.NewValue);
         }
-
-        /// <summary>
-        /// The row selected from the list
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="args"></param>
-        public void OnPopupItemSelected(object sender, SelectedItemChangedEventArgs args)
-        {
-            ItemModel data = args.SelectedItem as ItemModel;
-            if (data == null)
-            {
-                return;
-            }
-
-            ViewModel.Data.AddItem(PopupLocationEnum, data.Id);
-
-            AddItemsToDisplay();
-
-            ClosePopup();
-        }
-
-        /// <summary>
-        /// Show the Popup for Selecting Items
-        /// </summary>
-        /// <param name="location"></param>
-        /// <returns></returns>
-        public bool ShowPopup(ItemLocationEnum location)
-        {
-            PopupItemSelector.IsVisible = true;
-
-            PopupLocationLabel.Text = "Items for :";
-            PopupLocationValue.Text = location.ToMessage();
-
-            // Make a fake item for None
-            var NoneItem = new ItemModel
-            {
-                Id = null, // will use null to clear the item
-                Guid = "None", // how to find this item amoung all of them
-                ImageURI = "icon_cancel.png",
-                Name = "None",
-                Description = "None"
-            };
-
-            List<ItemModel> itemList = new List<ItemModel>
-            {
-                NoneItem
-            };
-
-            // Add the rest of the items to the list
-            itemList.AddRange(ItemIndexViewModel.Instance.GetLocationItems(location));
-
-            // Populate the list with the items
-            PopupLocationItemListView.ItemsSource = itemList;
-
-            // Remember the location for this popup
-            PopupLocationEnum = location;
-
-            return true;
-        }
-
-        /// <summary>
-        /// When the user clicks the close in the Popup
-        /// hide the view
-        /// show the scroll view
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void ClosePopup_Clicked(object sender, EventArgs e)
-        {
-            ClosePopup();
-        }
-
-        /// <summary>
-        /// Close the popup
-        /// </summary>
-        public void ClosePopup()
-        {
-            PopupItemSelector.IsVisible = false;
-        }
-
-        /// <summary>
-        /// Show the Items the Monster has
-        /// </summary>
-        public void AddItemsToDisplay()
-        {
-            var FlexList = ItemBox.Children.ToList();
-            foreach (var data in FlexList)
-            {
-                ItemBox.Children.Remove(data);
-            }
-
-            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Head));
-            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Necklass));
-            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.PrimaryHand));
-            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.OffHand));
-            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.RightFinger));
-            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.LeftFinger));
-            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Feet));
-        }
-
-        /// <summary>
-        /// Look up the Item to Display
-        /// </summary>
-        /// <param name="location"></param>
-        /// <returns></returns>
-        public StackLayout GetItemToDisplay(ItemLocationEnum location)
-        {
-            // Get the Item, if it exist show the info
-            // If it does not exist, show a Plus Icon for the location
-
-            // Defualt Image is the Plus
-            var ImageSource = "icon_add.png";
-
-            var data = ViewModel.Data.GetItemByLocation(location);
-            if (data == null)
-            {
-                data = new ItemModel { Location = location, ImageURI = ImageSource };
-            }
-
-            // Hookup the Image Button to show the Item picture
-            var ItemButton = new ImageButton
-            {
-                Style = (Style)Application.Current.Resources["ImageMediumStyle"],
-                Source = data.ImageURI
-            };
-
-            // Add a event to the user can click the item and see more
-            ItemButton.Clicked += (sender, args) => ShowPopup(data.Location);
-
-            // Add the Display Text for the item
-            var ItemLabel = new Label
-            {
-                Text = location.ToMessage(),
-                Style = (Style)Application.Current.Resources["ValueStyleMicro"],
-                HorizontalOptions = LayoutOptions.Center,
-                HorizontalTextAlignment = TextAlignment.Center
-            };
-
-            // Put the Image Button and Text inside a layout
-            var ItemStack = new StackLayout
-            {
-                Padding = 3,
-                Style = (Style)Application.Current.Resources["ItemImageBox"],
-                HorizontalOptions = LayoutOptions.Center,
-                Children = {
-                    ItemButton,
-                    ItemLabel
-                },
-            };
-
-            return ItemStack;
-        }
-
+              
         /// <summary>
         /// Randomize Monster Values and Items
         /// </summary>
@@ -336,17 +142,6 @@ namespace Game.Views
             ViewModel.Data.Attack = RandomPlayerHelper.GetAbilityValue();
             ViewModel.Data.Speed = RandomPlayerHelper.GetAbilityValue();
             ViewModel.Data.Defense = RandomPlayerHelper.GetAbilityValue();
-
-            // Randomize an Item for Location
-            ViewModel.Data.Head = RandomPlayerHelper.GetItem(ItemLocationEnum.Head);
-            ViewModel.Data.Necklass = RandomPlayerHelper.GetItem(ItemLocationEnum.Necklass);
-            ViewModel.Data.PrimaryHand = RandomPlayerHelper.GetItem(ItemLocationEnum.PrimaryHand);
-            ViewModel.Data.OffHand = RandomPlayerHelper.GetItem(ItemLocationEnum.OffHand);
-            ViewModel.Data.RightFinger = RandomPlayerHelper.GetItem(ItemLocationEnum.Finger);
-            ViewModel.Data.LeftFinger = RandomPlayerHelper.GetItem(ItemLocationEnum.Finger);
-            ViewModel.Data.Feet = RandomPlayerHelper.GetItem(ItemLocationEnum.Feet);
-
-            ViewModel.Data.MaxHealth = RandomPlayerHelper.GetHealth(ViewModel.Data.Level);
 
             UpdatePageBindingContext();
 
