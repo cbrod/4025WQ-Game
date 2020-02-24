47c3e217349b535f20f31b04dae3586ffd5f29ac
Mike Koenig <koenigm@seattleu.edu>
2020-02-24 11:02:40 -0800

    Updated Character Model and added CharacterModel UTs

--- Game/Game/Models/CharacterModel.cs
+++ Game/Game/Models/CharacterModel.cs
@@ -1,4 +1,7 @@
 ﻿
+using Game.ViewModels;
+using System;
+
 namespace Game.Models
 {
     /// <summary>
@@ -21,6 +24,68 @@ namespace Game.Models
             Description = "Happy Elf";
             Level = 1;
             ImageURI = "knight.png";
+        }
+
+        /// <summary>
+        /// Create a copy
+        /// </summary>
+        /// <param name="data"></param>
+        public CharacterModel(CharacterModel data)
+        {
+            Update(data);
+        }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        /// <param name="newData"></param>
+        /// <returns></returns>
+        public override bool Update(CharacterModel newData)
+        {
+            if (newData == null)
+            {
+                return false;
+            }
+
+            PlayerType = newData.PlayerType;
+            Guid = newData.Guid;
+            Name = newData.Name;
+            Description = newData.Description;
+            Level = newData.Level;
+            ImageURI = newData.ImageURI;
+
+            // Difficulty = newData.Difficulty;
+
+            Speed = newData.Speed;
+            Defense = newData.Defense;
+            Attack = newData.Attack;
+
+            ExperienceTotal = newData.ExperienceTotal;
+            ExperiencePoints = newData.ExperiencePoints;
+            CurrentHealth = newData.CurrentHealth;
+            MaxHealth = newData.MaxHealth;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Helper to combine the attributes into a single line, to make it easier to display the item as a string
+        /// </summary>
+        /// <returns></returns>
+        public override string FormatOutput()
+        {
+            var myReturn = string.Empty;
+            myReturn += Name;
+            myReturn += " , " + Description;
+            myReturn += " , Level : " + Level.ToString();
+            myReturn += " , Total Experience : " + ExperienceTotal;
+            myReturn += " , Attack " + GetAttackTotal;
+            myReturn += " , Defense " + GetDefenseTotal;
+            myReturn += " , Speed " + GetSpeedTotal;
+            myReturn += " , Items : " + ItemSlotsFormatOutput();
+            myReturn += " Damage : " + GetDamageTotalString;
+
+            return myReturn;
         }
     }
 }
