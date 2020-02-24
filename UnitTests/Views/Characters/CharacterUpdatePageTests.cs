c2a15b97952cd146fa7f708f1fe50b98368cc796
Mike Koenig <koenigm@seattleu.edu>
2020-02-23 22:56:02 -0800

    Added Close Popup test to Character Update Page

--- UnitTests/Views/Characters/CharacterUpdatePageTests.cs
+++ UnitTests/Views/Characters/CharacterUpdatePageTests.cs
@@ -7,6 +7,7 @@ using Game.Models;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Mocks;
+using System.Linq;
 
 namespace UnitTests.Views
 {
@@ -203,6 +204,86 @@ namespace UnitTests.Views
 
             // Act
             page.RollDice_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void CharacterUpdatePage_ClosePopup_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.ClosePopup();
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void CharacterUpdatePage_ClosePopup_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.ClosePopup_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void CharacterUpdatePage_OnPopupItemSelected_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            var data = new ItemModel();
+
+            var selectedCharacterChangedEventArgs = new SelectedItemChangedEventArgs(data, 0);
+
+            // Act
+            page.OnPopupItemSelected(null, selectedCharacterChangedEventArgs);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void CharacterUpdatePage_OnPopupItemSelected_Clicked_Null_Should_Fail()
+        {
+            // Arrange
+
+            var selectedCharacterChangedEventArgs = new SelectedItemChangedEventArgs(null, 0);
+
+            // Act
+            page.OnPopupItemSelected(null, selectedCharacterChangedEventArgs);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void CharacterUpdatePage_Item_ShowPopup_Default_Should_Pass()
+        {
+            // Arrange
+
+            var item = page.GetItemToDisplay(ItemLocationEnum.Head);
+
+            // Act
+            var itemButton = item.Children.FirstOrDefault(m => m.GetType().Name.Equals("Button"));
+
+            page.ShowPopup(ItemLocationEnum.Head);
 
             // Reset
 
